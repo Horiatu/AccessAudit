a210f7cf3625b30600f4bbe6c0d7d31c4bbb8950
Horia Tudosie <htudosie@esri.ca>
2017-11-20 14:20:32 -0500

    Added analytics

--- AccessAudit/inc/js/options.js
+++ AccessAudit/inc/js/options.js
@@ -1,4 +1,17 @@
 // On document load
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-109917224-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = 'https://ssl.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+})();
+
 $(document).ready(function() {
     // show tabs
     addCssClass('@font-face', 'font-family: "Poiret One";\n\t\tfont-weight: 400;\n\t\tsrc: url("'+chrome.extension.getURL("/fonts/Poiret One.woff2")+'") format("woff2");', 'fonts');
